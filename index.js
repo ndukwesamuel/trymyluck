c5b8086e4ac93bcabc02063c2ad098aa10535d66
ndukwesamuel <ndukwesamuel23@yahoo.com>
2022-06-02 10:02:53 +0100

    djfhdf

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("<h1> samheart</h1>");
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
+// app.get("/", (req, res) => {
+//   res.send("<h1> samheart</h1>");
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
